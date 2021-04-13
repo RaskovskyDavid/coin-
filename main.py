497ab0a341f48a23ad9720b53f4296968bca266b
David Raskovsky <>
2021-04-13 23:31:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+import random
+#"Heads" or "Tails".
+coin_value = random.randint(0,1)
+if coin_value == 0:
+  print("Tails")
+if coin_value == 1:
+  print("Heads")
+
+
+
+
+
+
+
+
